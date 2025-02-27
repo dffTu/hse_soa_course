f2a5a875f6dcb3855e69fd69c146dc9c1619b065
Timur Nasretdinov <timur.nasretdinov@ntprog.com>
2025-02-27 14:02:32 +0300

    Aaa

---
+++ gateway/db/config.py
@@ -0,0 +1,5 @@
+DB_NAME="gateway"
+DB_USER="admin"
+DB_PASSWORD="admin"
+DB_PORT=5432
+DB_HOST="localhost"
--- gateway/server/main.py
+++ gateway/server/main.py
@@ -4,6 +4,7 @@ import os
 from collections.abc import AsyncIterator
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import AsyncExitStack, asynccontextmanager
+import time
 
 import uvicorn
 from fastapi import FastAPI
@@ -14,11 +15,11 @@ from starlette.middleware.gzip import GZipMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
 
-import config
-from api import router
+import server.config as config
+from server.api import router
+from db.main import Database
 
 logger = logging.getLogger(__name__)
-
 
 async def validation_exception_handler(request: Request, exc: RequestValidationError) -> Response:
     logger.exception("Invalid request data: %s", exc)
@@ -41,6 +42,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+time.sleep(5)
+db = Database()
+time.sleep(5)
+db.add_something()
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
