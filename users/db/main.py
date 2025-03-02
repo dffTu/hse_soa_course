fee343984c97841bb36f1f2cd143c171608b1b95
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2025-03-02 22:47:26 +0300

    Added timestamp and fixed queries

--- users/db/main.py
+++ users/db/main.py
@@ -4,6 +4,7 @@ import psycopg2
 import time
 import os
 from typing import Union
+from datetime import datetime, timezone
 
 class Database:
     def __init__(self):
@@ -31,15 +32,17 @@ class Database:
             return False
 
         hashed_password = str(hash(password))
+        creation_timestamp = datetime.now(timezone.utc)
         
-        cursor.execute('INSERT INTO accounts DEFAULT VALUES RETURNING id')
+        cursor.execute('INSERT INTO accounts (creation_timestamp) VALUES (%s) RETURNING id',
+                       (creation_timestamp, ))
         account_id = cursor.fetchone()[0]
 
-        cursor.execute('INSERT INTO logins (account_id, login, hashed_password, is_freezed, login_tries_in_1h) ' \
-                       f'VALUES ({account_id}, \'{login}\', \'{hashed_password}\', FALSE, 0)')
+        cursor.execute('INSERT INTO logins (account_id, login, hashed_password, is_freezed, login_tries_in_1h) VALUES (%s, %s, %s, FALSE, 0)',
+                       (account_id, login, hashed_password))
         
-        cursor.execute('INSERT INTO user_info (account_id, email) ' \
-                       f'VALUES ({account_id}, \'{email}\')')
+        cursor.execute('INSERT INTO user_info (account_id, email) VALUES (%s, %s)',
+                       (account_id, email))
         
         self.__conn.commit()
 
@@ -49,7 +52,8 @@ class Database:
         cursor = self.__conn.cursor()
         hashed_password = str(hash(password))
 
-        cursor.execute(f'SELECT * FROM logins WHERE login = \'{login}\' AND hashed_password = \'{hashed_password}\'')
+        cursor.execute(f'SELECT * FROM logins WHERE login = %s AND hashed_password = %s',
+                       (login, hashed_password))
         logins = cursor.fetchall()
         if len(logins) == 0:
             return None
@@ -57,8 +61,8 @@ class Database:
         account_id = logins[0][0]
         token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
-        cursor.execute('INSERT INTO tokens (account_id, token) ' \
-                       f'VALUES ({account_id}, \'{token}\')')
+        cursor.execute('INSERT INTO tokens (account_id, token) VALUES (%s, %s)',
+                       (account_id, token))
         
         self.__conn.commit()
 
--- users/db/scripts/1_create_accounts_table.sql
+++ users/db/scripts/1_create_accounts_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS accounts (
     id SERIAL PRIMARY KEY,
     name VARCHAR(63),
     surname VARCHAR(63),
-    creation_date DATE,
+    creation_timestamp TIMESTAMP,
     role VARCHAR(63)
 )
